40c59deff790fa31abcce39a7b827573af333931
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-08-05 17:43:19 +0200

    ..

--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,10 +1,35 @@
 import PageNav from "../../Components/PageNav/PageNav";
-import Styels from "./Pricing.module.css";
+import { Link } from "react-router-dom";
+import Styles from "./Pricing.module.css";
 function Pricing() {
   return (
-    <div className={Styels.pricing}>
+    <div className={Styles.pricing}>
       <PageNav />
-      <h1>Pricing</h1>
+      <section className={Styles.planes}>
+        <div className={Styles.saveMoney}>
+          <h1 className={Styles.saveMoney__title}>
+            Save your <span> Money Now</span>
+          </h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque iusto commodi consectetur quibusdam? Doloribus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel similique repellendus ipsam quasi.</p>
+        </div>
+        <div className={Styles.title}>
+          <div className={Styles.title__header}>
+            <h1>Start now</h1>
+            <h1>
+              Your <span> Free Plan</span>
+            </h1>
+          </div>
+          <div className={Styles.title__readMore}>
+            <p>Are you intrusted?</p>
+            <Link to="/products" className={Styles.title__link}>
+              Read More
+            </Link>
+          </div>
+        </div>
+        <div className={Styles.freePlan}>free</div>
+        <div className={Styles.proPlan}>pro</div>
+      </section>
     </div>
   );
 }
