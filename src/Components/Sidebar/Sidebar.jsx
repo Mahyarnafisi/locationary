7ba189d60e4d045083a6ef95ccae1c45e5e1287e
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-08-11 21:45:51 +0200

    ..

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,13 +1,15 @@
 import Styles from "./Sidebar.module.css";
 import Logo from "../Logo/Logo";
 import Footer from "../Footer/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import AppNav from "../AppNav/AppNav";
 
 function Sidebar() {
   return (
     <div className={Styles.sidebar}>
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <AppNav />
       <Outlet />
       <Footer />
