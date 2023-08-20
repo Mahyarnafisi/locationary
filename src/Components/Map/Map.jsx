09bfed7d5cb60e6c64ba4d0ebf09b718a9394f49
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-08-20 21:46:36 +0200

    initiate multiple location and based on current city

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,20 +1,27 @@
 import { useSearchParams } from "react-router-dom";
-import { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import Styles from "./Map.module.css";
 import { useCities } from "../../Contexts/CitiesContext";
 function Map() {
   const { cities } = useCities();
   const [searchParams, setSearchParams] = useSearchParams();
-
   const [mapPosition, setMapPosition] = useState([55.7047, 13.191]);
+
+  let lat = searchParams.get("lat");
+  let lng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (lat && lng) setMapPosition([lat, lng]);
+  }, [lat, lng]);
+
   return (
     <div className={Styles.mapContainer}>
       <div className={Styles.locationCard}>
         <p>Positions: lat:- lng:</p>
       </div>
-      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={Styles.map}>
-        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <MapContainer center={mapPosition} zoom={12} scrollWheelZoom={true} className={Styles.map}>
+        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
 
         {cities.map((city) => {
           return (
@@ -31,9 +38,15 @@ function Map() {
             </Marker>
           );
         })}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
