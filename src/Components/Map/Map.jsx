9cc4e181aae0420b55045315a4035058cb2f4df8
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-08-20 16:42:52 +0200

    custom style popup marker in map

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,30 +1,36 @@
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import Styles from "./Map.module.css";
 import { useCities } from "../../Contexts/CitiesContext";
 function Map() {
-  const { currentCity } = useCities();
-
-  const navigate = useNavigate();
+  const { cities } = useCities();
   const [searchParams, setSearchParams] = useSearchParams();
-  let lat = searchParams.get("lat");
-  let lng = searchParams.get("lng");
+
   const [mapPosition, setMapPosition] = useState([55.7047, 13.191]);
   return (
     <div className={Styles.mapContainer}>
       <div className={Styles.locationCard}>
-        <p>
-          Positions: lat:{lat} - lng:{lng}
-        </p>
+        <p>Positions: lat:- lng:</p>
       </div>
-      <MapContainer center={mapPosition} zoom={14} scrollWheelZoom={true} className={Styles.map}>
+      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={Styles.map}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {cities.map((city) => {
+          return (
+            <Marker key={city.id} position={[city.position.lat, city.position.lng]} className={Styles.marker}>
+              <Popup className={Styles.popup}>
+                <div>
+                  <h4>{city.city_name}</h4>
+                  <div>
+                    <p>Comment:</p>
+                    <p>{city.comment}</p>
+                  </div>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
